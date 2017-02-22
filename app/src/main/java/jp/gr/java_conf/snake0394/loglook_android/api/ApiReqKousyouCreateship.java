752dcbc55defdc28c04214a896377850c96cfb37
yamase0394 <raiden1192@gmail.com>
2017-02-23 00:24:25 +0900

    リクエストボディをMapで扱うようにした

--- app/src/main/java/jp/gr/java_conf/snake0394/loglook_android/api/ApiReqKousyouCreateship.java
+++ app/src/main/java/jp/gr/java_conf/snake0394/loglook_android/api/ApiReqKousyouCreateship.java
@@ -2,9 +2,6 @@ package jp.gr.java_conf.snake0394.loglook_android.api;
 
 import com.google.gson.JsonObject;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import jp.gr.java_conf.snake0394.loglook_android.logger.CreateShipLogger;
 import jp.gr.java_conf.snake0394.loglook_android.proxy.RequestMetaData;
 import jp.gr.java_conf.snake0394.loglook_android.proxy.ResponseMetaData;
@@ -17,19 +14,8 @@ public class ApiReqKousyouCreateship implements APIListenerSpi {
     @Override
     public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
 
-        String requestBody = req.getRequestBody();
-
-        String regex = "Fkdock%5Fid=(\\d+)";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(requestBody);
-        m.find();
-        int kdockId = Integer.parseInt(m.group(1));
-
-        regex = "large%5Fflag=(\\d+)";
-        p = Pattern.compile(regex);
-        m = p.matcher(requestBody);
-        m.find();
-        int largeFlag = Integer.parseInt(m.group(1));
+        int kdockId = Integer.parseInt(req.getParameterMap().get("api_kdock_id").get(0));
+        int largeFlag = Integer.parseInt(req.getParameterMap().get("api_large_flag").get(0));
 
         CreateShipLogger.INSTANCE.ready(kdockId,largeFlag);
     }
--- app/src/main/java/jp/gr/java_conf/snake0394/loglook_android/api/ApiReqNyukyoSpeedchange.java
+++ app/src/main/java/jp/gr/java_conf/snake0394/loglook_android/api/ApiReqNyukyoSpeedchange.java
@@ -2,9 +2,6 @@ package jp.gr.java_conf.snake0394.loglook_android.api;
 
 import com.google.gson.JsonObject;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import jp.gr.java_conf.snake0394.loglook_android.DockTimer;
 import jp.gr.java_conf.snake0394.loglook_android.proxy.RequestMetaData;
 import jp.gr.java_conf.snake0394.loglook_android.proxy.ResponseMetaData;
@@ -17,12 +14,6 @@ public class ApiReqNyukyoSpeedchange implements APIListenerSpi {
     @Override
     public void accept(JsonObject json, RequestMetaData req, ResponseMetaData res) {
 
-        String requestBody = req.getRequestBody();
-
-        String regex = "api%5Fndock%5Fid=(\\d+)";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(requestBody);
-        m.find();
-        DockTimer.INSTANCE.stop(Integer.parseInt(m.group(1)));
+        DockTimer.INSTANCE.stop(Integer.parseInt(req.getParameterMap().get("api_ndock_id").get(0)));
     }
 }
